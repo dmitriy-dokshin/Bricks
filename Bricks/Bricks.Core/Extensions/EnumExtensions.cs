9423daebcaacbd9854f1cb5fd70c38ce84403126
Dmitriy Dokshin <dmitriy.dokshin@gmail.com>
2015-02-06 12:15:01 +0300

    [+] Some Enum simple extensions. [+] CommaDelimitedArrayModelBinder for Web API.

---
+++ Bricks/Bricks.Core/Extensions/EnumExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace Bricks.Core.Extensions
+{
+	public static class EnumExtensions
+	{
+		public static string GetName(this System.Enum value)
+		{
+			string name = System.Enum.GetName(value.GetType(), value);
+			return name;
+		}
+
+		public static string GetLowerCamelCaseName(this System.Enum value)
+		{
+			string name = value.GetName();
+			if (name.Length > 0 && !char.IsLower(name[0]))
+			{
+				name = char.ToLowerInvariant(name[0]) + (name.Length > 1 ? name.Substring(1) : string.Empty);
+			}
+
+			return name;
+		}
+	}
+}
---
+++ Bricks/Bricks.WebAPI/ModelBinders/CommaDelimitedArrayModelBinder.cs
@@ -0,0 +1,62 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Web.Http.Controllers;
+using System.Web.Http.ModelBinding;
+using System.Web.Http.ValueProviders;
+
+#endregion
+
+namespace Bricks.WebAPI.ModelBinders
+{
+	public class CommaDelimitedArrayModelBinder : IModelBinder
+	{
+		public bool BindModel(HttpActionContext actionContext, ModelBindingContext bindingContext)
+		{
+			string modelName = bindingContext.ModelName;
+			ValueProviderResult value = bindingContext.ValueProvider.GetValue(modelName);
+			if (value != null)
+			{
+				string attemptedValue = value.AttemptedValue;
+				Type modelType = bindingContext.ModelType;
+				Type elementType = null;
+				if (modelType.IsArray)
+				{
+					elementType = modelType.GetElementType();
+				}
+				else if (modelType.IsGenericType)
+				{
+					Type genericType = modelType.GetGenericTypeDefinition();
+					if (genericType == typeof(IEnumerable<>) || genericType == typeof(IReadOnlyCollection<>))
+					{
+						elementType = modelType.GetGenericArguments()[0];
+					}
+				}
+
+				if (elementType == null)
+				{
+					throw new InvalidOperationException(Resources.CommaDelimitedArrayModelBinder_BindModel_ArrayOrEnumerableOrReadOnlyCollectionRequired);
+				}
+
+				if (attemptedValue != null)
+				{
+					var converter = TypeDescriptor.GetConverter(elementType);
+					var values = Array.ConvertAll(attemptedValue.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries), x => converter.ConvertFromString(x != null ? x.Trim() : null));
+					var typedValues = Array.CreateInstance(elementType, values.Length);
+					values.CopyTo(typedValues, 0);
+					bindingContext.Model = typedValues;
+				}
+				else
+				{
+					bindingContext.Model = null;
+				}
+
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
