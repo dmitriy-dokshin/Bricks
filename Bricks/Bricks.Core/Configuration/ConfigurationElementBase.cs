561e7122d41ab33ad40c120cbab3c92614f1e5e9
dmitriy-dokshin <dmitriy.dokshin@gmail.com>
2015-05-07 14:24:53 +0300

    [~] Configuration base types.

---
+++ Bricks/Bricks.Core/Configuration/ConfigurationElementBase.cs
@@ -0,0 +1,44 @@
+﻿#region
+
+using System;
+using System.Configuration;
+using System.Linq.Expressions;
+
+using Bricks.Core.Extensions;
+using Bricks.Core.Reflection;
+
+#endregion
+
+namespace Bricks.Core.Configuration
+{
+	public abstract class ConfigurationElementBase : ConfigurationElement
+	{
+		public TValue GetValue<TValue>(Expression<Func<TValue>> expression)
+		{
+			string memberName = expression.GetMemberName();
+			return (TValue)this[memberName];
+		}
+
+		#region Overrides of ConfigurationElement
+
+		protected override void PostDeserialize()
+		{
+			foreach (ConfigurationProperty property in Properties)
+			{
+				var configElement = this[property] as ConfigurationElement;
+				if (configElement != null)
+				{
+					ElementInformation elementInformation = configElement.ElementInformation;
+					if (!elementInformation.IsPresent && elementInformation.Type.IsNullable())
+					{
+						this[property] = null;
+					}
+				}
+			}
+
+			base.PostDeserialize();
+		}
+
+		#endregion
+	}
+}
